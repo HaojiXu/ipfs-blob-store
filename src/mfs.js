0617be53600a89275d9e7335dc381ef55f03b2f6
Haoji Xu <haoji.xu@outlook.com>
2020-06-01 17:37:01 +0800

    Update mfs.js

--- src/mfs.js
+++ src/mfs.js
@@ -3,117 +3,118 @@
 const stream = require('stream')
 const path = require('path')
 const log = require('debug')('ipfs:blob-store:mfs')
+const a2s = require("async-iterator-to-stream");
 
-module.exports = function (options) {
-  const store = {}
-  store.baseDir = options.baseDir || '/'
+module.exports = function(options) {
+    const store = {}
+    store.baseDir = options.baseDir || '/'
 
-  if (!store.baseDir.endsWith('/')) {
-    store.baseDir += '/'
-  }
+    if (!store.baseDir.endsWith('/')) {
+        store.baseDir += '/'
+    }
 
-  const ipfs = options.ipfs
-  store.ipfs = ipfs
-  if (typeof options.flush === 'boolean' && options.flush === false) {
-    // let it as it is
-  } else {
-    options.flush = true
-  }
+    const ipfs = options.ipfs
+    store.ipfs = ipfs
+    if (typeof options.flush === 'boolean' && options.flush === false) {
+        // let it as it is
+    } else {
+        options.flush = true
+    }
 
-  store.createWriteStream = function (opts, cb) {
-    if (typeof opts === 'string') opts = {key: opts}
-    if (opts.name) opts.key = opts.name
-    if (!cb) cb = noop
+    store.createWriteStream = function(opts, cb) {
+        if (typeof opts === 'string') opts = { key: opts }
+        if (opts.name) opts.key = opts.name
+        if (!cb) cb = noop
 
-    const writePath = normalisePath(store.baseDir + opts.key)
-    const bufferStream = new stream.PassThrough()
+        const writePath = normalisePath(store.baseDir + opts.key)
+        const bufferStream = new stream.PassThrough()
 
-    let size = 0
+        let size = 0
 
-    let bufs = [];
+        let bufs = [];
 
-    bufferStream.on('data', (buffer) => {
-        bufs.push(buffer)
-        size += buffer.length
-    })
+        bufferStream.on('data', (buffer) => {
+            bufs.push(buffer)
+            size += buffer.length
+        })
 
-    bufferStream.on('end', () => {
-        let myBuf = Buffer.concat(bufs);
-        ipfs.files.write(writePath, myBuf, {
-                create: true,
-                parents: true,
-                flush: options.flush
-            })
-            .then(() => {
-                cb(null, {
-                    key: opts.key,
-                    size: size,
-                    name: path.basename(writePath)
+        bufferStream.on('end', () => {
+            let myBuf = Buffer.concat(bufs);
+            ipfs.files.write(writePath, myBuf, {
+                    create: true,
+                    parents: true,
+                    flush: options.flush
                 })
-            })
-            .catch(error => {
-                cb(error)
-            })
-    })
+                .then(() => {
+                    cb(null, {
+                        key: opts.key,
+                        size: size,
+                        name: path.basename(writePath)
+                    })
+                })
+                .catch(error => {
+                    cb(error)
+                })
+        })
 
-    return bufferStream
-  }
+        return bufferStream
+    }
 
-  store.createReadStream = function (opts) {
-    if (typeof opts === 'string') opts = {key: opts}
-    if (opts.name) opts.key = opts.name
+    store.createReadStream = function(opts) {
+        if (typeof opts === 'string') opts = { key: opts }
+        if (opts.name) opts.key = opts.name
 
-    const readPath = normalisePath(store.baseDir + opts.key)
+        const readPath = normalisePath(store.baseDir + opts.key)
 
-    log(`read ${readPath}`)
-    const readableStream = ipfs.files.readReadableStream(readPath)
+        log(`read ${readPath}`)
+        const readableStream = a2s(ipfs.files.read(readPath));
 
-    readableStream.on('error', (error) => {
-      if (error.toString().indexOf('does not exist') > -1 || error.toString().indexOf('Not a directory') > -1) {
-        error.notFound = true
-      }
-    })
+        readableStream.on('error', (error) => {
+            if (error.toString().indexOf('does not exist') > -1 || error.toString().indexOf('Not a directory') > -1) {
+                error.notFound = true
+            }
+        })
 
-    return readableStream
-  }
+        return readableStream
+    }
 
-  store.exists = function (opts, cb) {
-    if (typeof opts === 'string') opts = {key: opts}
-    if (opts.name) opts.key = opts.name
-    if (!cb) cb = noop
+    store.exists = function(opts, cb) {
+        if (typeof opts === 'string') opts = { key: opts }
+        if (opts.name) opts.key = opts.name
+        if (!cb) cb = noop
 
-    const statPath = normalisePath(store.baseDir + opts.key)
+        const statPath = normalisePath(store.baseDir + opts.key)
 
-    log(`stat ${statPath}`)
-    ipfs.files.stat(statPath, {}, (error) => {
-      if (error) {
-        if (error.toString().indexOf('does not exist') > -1 || error.toString().indexOf('Not a directory') > -1) {
-          return cb(null, false)
-        }
+        log(`stat ${statPath}`)
+        ipfs.files.stat(statPath, {}, (error) => {
+            if (error) {
+                if (error.toString().indexOf('does not exist') > -1 || error.toString().indexOf('Not a directory') > -1) {
+                    return cb(null, false)
+                }
 
-        return cb(error)
-      }
+                return cb(error)
+            }
 
-      cb(null, true)
-    })
-  }
+            cb(null, true)
+        })
+    }
 
-  store.remove = function (opts, cb) {
-    if (typeof opts === 'string') opts = {key: opts}
-    if (opts.name) opts.key = opts.name
-    if (!cb) cb = noop
+    store.remove = function(opts, cb) {
+        if (typeof opts === 'string') opts = { key: opts }
+        if (opts.name) opts.key = opts.name
+        if (!cb) cb = noop
 
-    const rmPath = normalisePath(store.baseDir + opts.key)
+        const rmPath = normalisePath(store.baseDir + opts.key)
 
-    log(`rm ${rmPath}`)
-    ipfs.files.rm(rmPath, cb)
-  }
+        log(`rm ${rmPath}`)
+        ipfs.files.rm(rmPath, cb)
+    }
 
-  return store
+    return store
 }
 
-function noop () {}
+function noop() {}
 
-function normalisePath (path) {
-  return path.replace(/\/(\/)+/g, '/')
+function normalisePath(path) {
+    return path.replace(/\/(\/)+/g, '/')
 }
